e9119be6353d5342f9bc33d93cf7c225565a7f0c
gonzalo fernandez <gonchjj.14@gmail.com>
2023-10-26 23:10:09 -0300

    add some files, page errors , carrousel

--- controllers/canciones.js
+++ controllers/canciones.js
@@ -20,5 +20,32 @@ const allGeneros = async (req,res) =>{
   }
 }
 
+const allMoods = async (req,res) =>{
+  try {
+    const resultado = await knex("estadosdeanimo");
+    res.json(resultado);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+}
 
-  module.exports = {allCanciones, allGeneros}
+const allClima = async (req,res) =>{
+  try {
+    const resultado = await knex("climas");
+    res.json(resultado);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+}
+
+const allActividades = async (req,res) =>{
+  try {
+    const resultado = await knex("actividades");
+    res.json(resultado);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+}
+
+
+  module.exports = {allCanciones, allGeneros, allActividades, allClima, allMoods}
--- routes/canciones.js
+++ routes/canciones.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { allCanciones, allGeneros } = require("../controllers/canciones");
+const { allCanciones, allGeneros, allActividades, allClima, allMoods } = require("../controllers/canciones");
 const { verifyToken } = require("../middlewares/auth");
 
 const router = express.Router();
@@ -8,5 +8,8 @@ const router = express.Router();
 
 router.get("/canciones",allCanciones);
 router.get("/generos",allGeneros);
+router.get("/actividades",allActividades);
+router.get("/climas",allClima);
+router.get("/estadosdeanimo",allMoods);
 
 module.exports = router;
